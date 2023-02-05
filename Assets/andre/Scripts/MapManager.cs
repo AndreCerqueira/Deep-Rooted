5ab348806e31684b0811164f0255f913d0128a57
andre cerqueira <andrecerq16@gmail.com>
2023-02-05 12:11:11 +0000

    .

--- Assets/andre/Scripts/MapManager.cs
+++ Assets/andre/Scripts/MapManager.cs
@@ -11,24 +11,72 @@ public class MapManager : MonoBehaviour
     [SerializeField] 
     private TileBase dirt;
     [SerializeField]
+    private TileBase root;
+    [SerializeField]
     private Tilemap tileMap;
+    [SerializeField]
+    GameObject player;
     public int width;
     public int height;
 
+    private int initialPositionY = 0;
+    private int finalPositionY = -19;
 
+    
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CreateTiles());
-        StartCoroutine(DestroyTiles());
+        // Starting chunk goes from y = 0 to y = -19
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get distance between player and the finalPositionY
+        Vector3 playerPosition = player.transform.position;
+        Vector3Int playerPositionInTilemap = tileMap.WorldToCell(playerPosition);
+        int distanceToFinalPosition = playerPositionInTilemap.y - finalPositionY;
+
+        // Get distance between player and initialPositionY
+        int distanceToInitialPosition = playerPositionInTilemap.y - initialPositionY;
+
+        // Draw dirt tile at finalPositionY if distance is less than 20
+        if (distanceToFinalPosition < 20)
+        {
+            DrawDirtLine(finalPositionY);
+            finalPositionY -= 1;
+        }
+
+        // Remove dirt tile at initialPositionY if distance is greater than 20
+        if (distanceToInitialPosition < -20)
+        {
+            RemoveDirtLine(initialPositionY);
+            initialPositionY -= 1;
+        }
     }
+
+
+    private void DrawDirtLine(int y)
+    {
+        for (int x = -width; x < width; x++)
+        {
+            tileMap.SetTile(new Vector3Int(x, y, 0), dirt);
+        }
+    }
+
+
+    private void RemoveDirtLine(int y)
+    {
+        for (int x = -width; x < width; x++)
+        {
+            tileMap.SetTile(new Vector3Int(x, y, 0), null);
+        }
+    }
+
+
+    /*
 
     // Corroutine to create tiles in the tilemap from -width to width and -height to height
     IEnumerator CreateTiles()
@@ -69,5 +117,5 @@ public class MapManager : MonoBehaviour
         {
             tileMap.SetTile(new Vector3Int(x, lastY, 0), null);
         }
-    }
+    }*/
 }
